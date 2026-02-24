5985c7b81604f19eab049667f2acfb707214a09e
Richard Nguyen <rtrichardnguyen@gmail.com>
2026-02-24 13:18:05 -0500

    regression v1

--- download.py
+++ download.py
@@ -3,9 +3,6 @@ import pandas as pd
 
 from datetime import timedelta
 
-ticker = yf.Ticker("AMD")
-df = ticker.history(interval='1d', start=(pd.Timestamp.now(tz='UTC') - timedelta(weeks=260)), auto_adjust=False)
-
 COLUMN_MAP = {
     'Date': 'datetime',
     'Open': 'open',
@@ -14,12 +11,20 @@ COLUMN_MAP = {
     'Volume': 'volume'
 }
 
-df.drop(columns=['Dividends', 'Stock Splits', 'High', 'Low'], inplace=True)
 
-df.rename(columns=COLUMN_MAP, inplace=True)
-df.index.name = 'datetime'
+class Download(object):
 
-df.to_csv("AMD.csv")
+    def __init__(self, symbol_list):
 
-print(df.columns)
-print(df.index)
+        self._download(symbol_list)
+
+    def _download(self, symbol_list):
+        for s in symbol_list:
+            ticker = yf.Ticker(s)
+            df = ticker.history(interval='1d', start=(pd.Timestamp.now(tz='UTC') - timedelta(weeks=260)), auto_adjust=False)
+            df.drop(columns=['Dividends', 'Stock Splits', 'High', 'Low'], inplace=True)
+
+            df.rename(columns=COLUMN_MAP, inplace=True)
+            df.index.name = 'datetime'
+            df.to_csv(f"{s}.csv")
+
--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,5 @@
-from datetime import datetime as datetime
+from datetime import datetime as dt
+from datetime import timedelta
 
 import numpy as np
 import pandas as pd
@@ -9,13 +10,64 @@ from backtest import Backtest
 from data import HistoricCSVDataHandler
 from execution import SimulatedExecutionHandler
 from portfolio import Portfolio
+from download import Download
 
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+import yfinance as yf
+import pandas as pd
 
 class LinearRegressionStrategy(Strategy):
 
     def __init__(self, bars, events):
+
         self.bars = bars
         self.events = events
 
         self.symbol_list = self.bars.symbol_list
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+
+        bought = {}
+
+        for symbol in self.symbol_list:
+            bought['symbol'] = 'OUT'
+
+        return bought
+
+    def calculate_signals(self, event):
+
+        if event.type == 'MARKET':
+            spy = self.symbol_list[0]
+            nvda = self.symbol_list[1]
+
+            X = self.bars.get_latest_bar_values(spy, 'adj_close', N=1000).reshape(-1, 1)
+            Y = self.bars.get_latest_bar_values(nvda, 'adj_close', N=1000)
+            model = LinearRegression()
+            model.fit(X, Y)
+
+            Y_pred = model.predict(X)
+
+            plt.figure(figsize=(8, 6))
+            plt.scatter(X, Y, color='blue', label='Data Points')
+            plt.plot(X, Y_pred, color='red', linewidth='2', label='Linear Regression')
+            plt.legend()
+            plt.grid(True)
+            plt.xlabel('X')
+            plt.ylabel('Y')
+            plt.show()
+
+if __name__ == '__main__':
+
+    csv_dir = './'
+    symbol_list = ['SPY', 'NVDA']
+    initial_capital = 1000000
+    heartbeat = 0.0
+    start_date = dt(2026, 2, 24, 0, 0, 0)
+
+    Download(symbol_list)
+
+    backtest = Backtest(csv_dir, symbol_list, initial_capital, heartbeat, start_date, HistoricCSVDataHandler, SimulatedExecutionHandler, Portfolio, LinearRegressionStrategy)
+    backtest.simulate_trading()
